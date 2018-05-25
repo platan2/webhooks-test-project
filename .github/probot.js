6a60c013c01b5d0798d41a74f0df013383834879
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2018-05-25 21:59:05 +0200

    Test 1

--- .github/probot.js
+++ .github/probot.js
@@ -3,7 +3,7 @@ on('pull_request.closed')
   .comment(`This pull request was merged to [master](https://github.com/badges/shields/tree/master) branch. Now this change is waiting for deployment. 
   Deploys usually happen every few weeks. After deployment changes are copied to [gh-pages](https://github.com/badges/shields/tree/gh-pages) branch. 
   
-  This badge displays deployment status
+  This badge displays deployment status:
   ![](https://img.shields.io/badge/deploy%20status-not%20in%20gh--pages-yellow.svg)
 
   https://img.shields.io/github/commit-status/gh-pages/{{ pull_request.merge_commit_sha }}?label=deploy%20status`)
